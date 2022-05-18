2d973575834ed68cec9eea6399ed7f8fbec3530e
Erick Tsafack <ericktsafack2017@gmail.com>
2022-05-18 11:22:57 +0100

    ceci est

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Users} from "./users/users.model"
+import { AddressController } from './address/address.controller';
+import { AddressModule } from './address/address.module';
+import { Address } from "./address/address.model";
+
+@Module({
+  imports: [UsersModule, AuthModule, SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test2',
+    models: [Users, Address],
+    autoLoadModels: true,
+  }), AddressModule,],
+  controllers: [AppController, AddressController],
+  providers: [AppService],
+})
+export class AppModule {}
